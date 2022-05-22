cd22c37389e1835da9b27e02e6d4f20308b3e470
JoeyShelby <2276852293@qq.com>
2022-05-22 17:58:00 +0800

    update employee information completed

--- src/main/java/org/joey/controller/EmployeeController.java
+++ src/main/java/org/joey/controller/EmployeeController.java
@@ -11,10 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashMap;
@@ -89,5 +86,40 @@ public class EmployeeController {
             return Msg.fail().add("error", "员工名重复");
         }
 
+    }
+
+    /*根据id 获得员工信息*/
+    @RequestMapping(value = "/employee/{id}",method = RequestMethod.GET)
+    @ResponseBody
+    public Msg getEmployeeById(@PathVariable Integer id) {
+        Employee employeeById = employeeService.getEmployeeById(id);
+        if (employeeById != null) {
+            return Msg.success().add("employee",employeeById);
+        }
+        return Msg.fail();
+    }
+
+    @RequestMapping(value = "/employee/{empId}",method = RequestMethod.PUT)
+    @ResponseBody
+    public Msg updateEmployeeById(@PathVariable Integer empId, @Valid Employee employee, BindingResult result) {
+        // jsr303 检测不通过
+        if (result.hasErrors()) {
+            List<FieldError> fieldErrors = result.getFieldErrors();
+            Map<String, String> errors = new HashMap<>();
+            for (FieldError error : fieldErrors) {
+                String field = error.getField();
+                String defaultMessage = error.getDefaultMessage();
+                errors.put(field,defaultMessage);
+                System.out.println(field+":"+defaultMessage);
+            }
+
+            return Msg.fail().add("errors",errors);
+        }
+        System.out.println("update:"+employee);
+        if(employeeService.updateEmployee(employee) < 1){
+            return Msg.fail();
+        }
+
+        return Msg.success().add("employee",employee);
     }
 }
